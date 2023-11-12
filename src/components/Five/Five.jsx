e95b05d0eb58f392e4a44cc034a584abe2246add
Bimal Ray <72511459+zenn99-arch@users.noreply.github.com>
2023-11-12 09:31:34 +0530

    chnaged

--- src/components/Five/Five.jsx
+++ src/components/Five/Five.jsx
@@ -9,18 +9,16 @@ const Five = () => {
   loop
   src="https://assets2.lottiefiles.com/packages/lf20_vd2u9dwv.json"></lottie-player>
     <div className="end-logo">Ranboo Fashion</div>
-      <a href="https://www.instagram.com/chaniru.lk/" className="link-foot">Shop all</a><br />
+      <a href="" className="link-foot">Shop all</a><br />
       <br />
-      <a href="https://www.instagram.com/chaniru.lk/" className="link-foot">about</a><br />
+      <a href="" className="link-foot">about</a><br />
       <br />
-      <a href="https://www.instagram.com/chaniru.lk/" className="link-foot">contact</a><br />
+      <a href="" className="link-foot">contact</a><br />
       <br />
-      <a href="https://www.instagram.com/chaniru.lk/" className="link-foot">Instagram</a><br />
+      <a href="" className="link-foot">Instagram</a><br />
       <br />
-      <a href="https://www.instagram.com/chaniru.lk/" className="link-foot">FAQ</a>
-      <div className="credit">
-        <a href="https://www.instagram.com/chaniru.lk/" className="credit-hover">Crafted by - @chaniru.lk</a>
-      </div>
+      <a href="" className="link-foot">FAQ</a>
+     
   </div>;
 };
 
